2837ec02b88989466e1cdabedf19534abb38833a
NuriddinovMA <nuriddinov@bionet.nsc.ru>
2023-09-19 17:30:53 +0700

    heterozygous

--- charm_func/global_func.py
+++ charm_func/global_func.py
@@ -1,8 +1,8 @@
 import os
 
 def boolean(x):
-	if x.lower() in ['false','no','n'] : return False
-	elif x.lower() in ['true','yes','y']: return True
+	if str(x).lower() in ['false','no','n'] : return False
+	elif str(x).lower() in ['true','yes','y']: return True
 	else: return x
 
 def printlog(str, logname):

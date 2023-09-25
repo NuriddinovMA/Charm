3422dc76f03991a00d2703300d30ce8a01ff2fb2
NuriddinovMA <nuriddinov@bionet.nsc.ru>
2023-09-25 18:52:04 +0700

    svs_hic

--- charm_func/sv_maps.py
+++ charm_func/sv_maps.py
@@ -13,31 +13,34 @@ def generate_SV_map(chrom_sizes, resolution, rearrangement_list, work_dir, rname
 	shift = resolution//2
 	ChrmSzs = gf.ChromSizes(chrom_sizes,resolution)
 	ChrmIdx = gf.ChromIndexing(chrom_sizes)
-
+	
 	with open(rearrangement_list, 'r') as f: lines = f.readlines()
 	for line in lines:
-		cnt,mut,c1,p11,p12,a,c2,cnv1,cnv2 = line.split()[:9]
+		cnt,mut,c1,p11,p12,a,c2,cnv2 = line.split()[:8]
 		if (rname == False or rname == mut or stand_alone) and (line[0] != '#'):
 			gf.printlog('\t\tstructural variation %s' % line.strip(),log_file)
 			
 			if a in ['->','!>']: 
 				pointviews = ''
-				chrm_from, chrm_to, add_from, add_to = set([]),set([]),set([]),set([])
+				chrm_from, chrm_to, cnv, add_from, add_to = set([]),set([]),set([]),set([]),set([])
 				cc_from,cc_to,MT,mutChrmSzs = {},{},{},{}
-			pointviews += '%s %s %s\n' % (c1,p11,p12)
-			
-			cnv1,cnv2 = int(cnv1),int(cnv2)
-			
-			try: cc_from[c2].add(c1)
-			except KeyError: cc_from[c2] = set([c1,])
-			
-			try: cc_to[c1].add(c2)
-			except KeyError: cc_to[c1] = set([c2,])
+				mutChrmIdx = {i:ChrmIdx[i] for i in ChrmIdx}
+				mutChrmSzs = {c2:ChrmSzs[c2] for c2 in ChrmSzs}
+				
+				
+			idxStart = len(mutChrmIdx) + 1
+			try: mutChrmIdx[c2]
+			except KeyError:
+				mutChrmIdx[c2] = idxStart
+				mutChrmIdx[idxStart] = c2
 
-			if (c2 not in ChrmSzs) or ((cnv1+abs(cnv2)) != 1):
-				for chrm in ChrmSzs: 
-					add_from.add((c1,chrm))
-					add_to.add((c2,chrm))
+			pointviews += '%s %s %s\n' % (c1,p11,p12)
+			cnv2 = int(cnv2)
+			
+			try: cc_from[c1].add(c2)
+			except KeyError: cc_from[c1] = set([c2,])
+			try: cc_to[c2].add(c1)
+			except KeyError: cc_to[c2] = set([c1,])
 			
 			p11 = (int(p11)+shift)//resolution
 			try: p12 = (int(p12)+shift)//resolution
@@ -51,39 +54,54 @@ def generate_SV_map(chrom_sizes, resolution, rearrangement_list, work_dir, rname
 				except KeyError: MT[c2] = [(c1,i) for i in range(p12-1,p11-1,-1)]*abs(cnv2)
 			else: pass
 			
+			try: del mutChrmSzs[c1]
+			except KeyError: pass
 			try: mutChrmSzs[c2] += (p12-p11)*abs(cnv2)
 			except KeyError: mutChrmSzs[c2] = (p12-p11)*abs(cnv2)
 			
 			if a in ['->','>!']:
 				svf.markgenerate(MT,resolution,(cnt,mut),outdir)
 				
-				print(cc_from)
-				for c2 in cc_from:
-					print('1',cc_from[c2])
-					cc_from[c2] = list(cc_from[c2])
-					cc_from[c2].sort()
-					print('1',cc_from[c2])
-					for i in range(len(cc_from[c2])):
-						for j in range(i,len(cc_from[c2])): 
-							chrm_from.add((cc_from[c2][i],cc_from[c2][j]))
-							if i != j: 
-								print('2',(cc_from[c2][j],cc_from[c2][i]))
-								chrm_from -= set([(cc_from[c2][j],cc_from[c2][i])])
-							add_from -= set([(cc_from[c2][i],cc_from[c2][j]),(cc_from[c2][j],cc_from[c2][i])])
+				chrm_from, chrm_to = set([]),set([])
+				for c1 in cc_from:
+					lenc = len(cc_from[c1])
+					cc_from[c1] = list(cc_from[c1])
+					for i in range(lenc):
+						for j in range(i,lenc):
+							c2i,c2j = cc_from[c1][i],cc_from[c1][j]
+							if mutChrmIdx[c2i] <= mutChrmIdx[c2j]: chrm_to.add((c2i,c2j))
+							else: chrm_to.add((c2j,c2i))
+				print('to',chrm_to)
+				for c2 in cc_to:
+					lenc = len(cc_to[c2])
+					cc_to[c2] = list(cc_to[c2])
+					for i in range(lenc):
+						for j in range(i,lenc): 
+							c1i,c1j = cc_to[c2][i],cc_to[c2][j]
+							if ChrmIdx[c1i] <= ChrmIdx[c1j]: chrm_from.add((c1i,c1j))
+							else: chrm_from.add((c1j,c1i))
+				print('from',chrm_from)
 				
-				print(cc_to)
-				for c1 in cc_to:
-					cc_to[c1] = list(cc_to[c1])
-					print('3',cc_to[c1])
-					for i in range(len(cc_to[c1])):
-						for j in range(i,len(cc_to[c1])):
-							chrm_to.add((cc_to[c1][i],cc_to[c1][j]))
-							if i != j: chrm_to -= set([(cc_to[c1][j],cc_to[c1][i])])
-							add_to -= set([(cc_to[c1][i],cc_to[c1][j]),(cc_to[c1][j],cc_to[c1][i])])
+				full_bins_from,full_bins_to = [],[]
+				for c1 in cc_from: full_bins_from = [(c1,i) for i in range( ChrmSzs[c1] )]
+				for c2 in cc_to: full_bins_to.extend(MT[c2])
+				unique = np.unique(full_bins_to,return_counts=True,axis=0)
+				cnvBins = unique[0][unique[1]>1].tolist()+list(set(full_bins_from)-set(full_bins_to))
 				
-				for c2 in ChrmSzs:
-					try: mutChrmSzs[c2]
-					except KeyError: mutChrmSzs[c2] = ChrmSzs[c2]
+				if len(cnvBins) > 0:
+					print('!!!CNV!!!')
+					for i in cnvBins: cnv.add(i[0])
+					for c1 in cnv:
+						for chrm in ChrmSzs: 
+							if ChrmIdx[c1] <= ChrmIdx[chrm]: add_from.add((c1,chrm))
+							else: add_from.add((chrm,c1))
+						for c2 in cc_from[c1]: 
+							for chrm in mutChrmSzs: 
+								if mutChrmIdx[c2] <= mutChrmIdx[chrm]: add_to.add((c2,chrm))
+								else: add_to.add((chrm,c2))
+				else: print('!!!NO-CNV!!!')
+				print('from',add_from)
+				print('to',add_to)
 				
 				with open('%s/%s.%s.chr.sizes' % (outdir, mut, cnt), 'w') as f: 
 					Keys = sorted(mutChrmSzs)
